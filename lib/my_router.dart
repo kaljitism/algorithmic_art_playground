d85ac31c5603a8051068805d4c3cbef30b45caf7
Aditya Dwivedi <dwikaljit@gmail.com>
2024-01-28 12:51:38 +0530

    wrote rotating triangular blob field.

--- lib/my_router.dart
+++ lib/my_router.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:generative_art_playground/blob_field.dart';
 import 'package:generative_art_playground/fading_object.dart';
 import 'package:generative_art_playground/moving_particles.dart';
+import 'package:generative_art_playground/rotating_triangular_blob_field.dart';
 
 class MyRouter extends StatefulWidget {
   const MyRouter({super.key});
@@ -50,11 +50,11 @@ class _MyRouterState extends State<MyRouter> {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => const BlobField(),
+                  builder: (context) => const RotatingTriangularBlobField(),
                 ),
               );
             },
-            title: const Text('Moving Particles in a Field'),
+            title: const Text('Rotating Triangular Blob Field'),
           ),
         ],
       ),
